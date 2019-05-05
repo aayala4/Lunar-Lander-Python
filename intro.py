20b0148660ec45d31dc923180110bef2d8e70628
Patrick Falvey <patrickjafalvey@gmail.com>
2019-05-04 20:51:40 -0400

    left and right arrows work

--- intro.py
+++ intro.py
@@ -4,35 +4,35 @@ lander.topleft = 0,0
 WIDTH = 1400
 HEIGHT = 800
 
+ang = 0
 
 def draw():
 	screen.clear()
 	lander.draw()
-	lander.angle = 0
+	lander.angle = ang
 
-#def update():
-	#lander.right += 2
-	#if lander.right >= WIDTH:
-	#	lander.left = 0
+def update():
+	lander.right += 2
+	if lander.right >= WIDTH:
+		lander.left = 0
 
 def on_mouse_down(pos):
 	if lander.collidepoint(pos):
 		lander.left = 0
 
 def on_key_down(key):
+	global ang
 	if key.name == 'LEFT':
-		print("landerang 2: " + str(lander.angle))
-		rotate(lander.angle+13)
+		rotate(ang+13)
 	elif key.name == 'RIGHT':
-		print("landerang 2: " + str(lander.angle))
-		rotate(lander.angle-13)
+		rotate(ang-13)
 
 def rotate(newAng):
-	print("newAng: " + str(newAng))
+	global ang
 	if newAng > 90:
-		lander.angle = 90
+		ang = 90
 	elif newAng < -90:
-		lander.angle = -90
+		ang = -90
 	else:
-		lander.angle = newAng
-	print("landerang: " + str(lander.angle))
+		ang = newAng
+	lander.angle = ang
